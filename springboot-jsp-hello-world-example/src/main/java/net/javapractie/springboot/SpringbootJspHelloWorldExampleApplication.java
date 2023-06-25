b3251eedeafa02a277a5145bb5e061faf0096b88
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2023-06-25 13:20:21 +0530

    Add files via upload

---
+++ springboot-jsp-hello-world-example/src/main/java/net/javapractie/springboot/SpringbootJspHelloWorldExampleApplication.java
@@ -0,0 +1,15 @@
+package net.javapractie.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringbootJspHelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJspHelloWorldExampleApplication.class, args);
+	}
+
+}
+ 
---
+++ springboot-jsp-hello-world-example/src/test/java/net/javapractie/springboot/SpringbootJspHelloWorldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javapractie.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJspHelloWorldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
